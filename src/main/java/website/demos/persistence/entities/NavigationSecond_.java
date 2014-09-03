4b9ef7924d2b4ab3953655e8e9806a75aa9168ce
wenjie.guo <wenjie.guo@cn-wenjie-guo.sh.os.local>
2014-09-03 18:02:29 +0800

    20140903

--- src/main/java/website/demos/persistence/entities/NavigationSecond_.java
+++
@@ -1,9 +0,0 @@
-package website.demos.persistence.entities;
-
-import javax.annotation.Generated;
-import javax.persistence.metamodel.StaticMetamodel;
-
-@Generated(value="Dali", date="2014-08-13T15:59:33.765+0800")
-@StaticMetamodel(NavigationSecond.class)
-public class NavigationSecond_ extends Navigation_ {
-}
--- src/main/java/website/demos/web/controllers/PageController.java
+++ src/main/java/website/demos/web/controllers/PageController.java
@@ -16,20 +16,30 @@ public class PageController{
 	@RequestMapping("admin-login")
 	public String pageAdminLogin() {
 		logger.info("Page AdminLogin.html");
-		
 		return "AdminLogin";
 	}
+	
 	@RequestMapping("admin-control-panel")
 	public String pageAdminControlPanel() {
 		logger.info("Page AdminControlPanel.html");
-		
 		return "AdminControlPanel";
+	}
+	
+	@RequestMapping("admin-cp-left")
+	public String pageAdminCPLeft() {
+		logger.info("Page pageAdminCPLeft.html");
+		return "AdminCPLeft";
+	}
+	
+	@RequestMapping("admin-cp-main")
+	public String pageAdminCPMain() {
+		logger.info("Page AdminCPMain.html");
+		return "AdminCPMain";
 	}
 	
 	@RequestMapping("rest")
 	public String pageRest() {
 		logger.info("Page rest.htm");
-		
 		return "rest";
 	}
 
